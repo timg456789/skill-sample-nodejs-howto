17f194b27265469153691a83e9ffff1209acac95
Timothy Gonzalez <timg456789@yahoo.com>
2018-02-04 15:19:26 -0500

    Use data from SlideshowCreator project for alexa responses to name works of art by an artist. This is the first step in showing this art.
    "art gallery"
    "show works by renoir"
    "show paintings by renoir"
    "show images by renoir"
    etc...

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -19,37 +19,36 @@ const languageStrings = {
     'en': {
         translation: {
             RECIPES: recipes.RECIPE_EN_US,
-            // TODO: Update these messages to customize.
-            SKILL_NAME: 'Minecraft Helper',
-            WELCOME_MESSAGE: "Welcome to %s. You can ask a question like, what\'s the recipe for a chest? ... Now, what can I help you with?",
+            SKILL_NAME: 'art gallery',
+            WELCOME_MESSAGE: "Welcome to %s. You can ask a question like, show works by renoir. ... Now, what can I help you with?",
             WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
             DISPLAY_CARD_TITLE: '%s  - Recipe for %s.',
-            HELP_MESSAGE: "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, what can I help you with?",
-            HELP_REPROMPT: "You can say things like, what\'s the recipe, or you can say exit...Now, what can I help you with?",
+            HELP_MESSAGE: "You can ask a question like, show works by renoir. ... Now, what can I help you with?",
+            HELP_REPROMPT: "You can ask a question like, show works by renoir. ... Now, what can I help you with?",
             STOP_MESSAGE: 'Goodbye!',
             RECIPE_REPEAT_MESSAGE: 'Try saying repeat.',
             RECIPE_NOT_FOUND_MESSAGE: "I\'m sorry, I currently do not know ",
-            RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'the recipe for %s. ',
-            RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that recipe. ',
+            RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'the work by %s. ',
+            RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that work of art. ',
             RECIPE_NOT_FOUND_REPROMPT: 'What else can I help with?',
         },
     },
     'en-US': {
         translation: {
             RECIPES: recipes.RECIPE_EN_US,
-            SKILL_NAME: 'American Minecraft Helper',
+            SKILL_NAME: 'American art gallery',
         },
     },
     'en-GB': {
         translation: {
             RECIPES: recipes.RECIPE_EN_GB,
-            SKILL_NAME: 'British Minecraft Helper',
+            SKILL_NAME: 'British art gallery',
         },
     },
     'de': {
         translation: {
             RECIPES: recipes.RECIPE_DE_DE,
-            SKILL_NAME: 'Assistent für Minecraft in Deutsch',
+            SKILL_NAME: 'Assistent für Art Gallery in Deutsch',
             WELCOME_MESSAGE: 'Willkommen bei %s. Du kannst beispielsweise die Frage stellen: Welche Rezepte gibt es für eine Truhe? ... Nun, womit kann ich dir helfen?',
             WELCOME_REPROMPT: 'Wenn du wissen möchtest, was du sagen kannst, sag einfach „Hilf mir“.',
             DISPLAY_CARD_TITLE: '%s - Rezept für %s.',
