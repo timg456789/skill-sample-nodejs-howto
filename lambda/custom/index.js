60b8dbf773c1d73338014e3adcaad07244f7d98a
Timothy Gonzalez <timg456789@yahoo.com>
2018-02-15 19:20:11 -0500

    everything works well, except alexa literally says "percent s" for %s

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,26 +1,31 @@
 'use strict';
-
 const Alexa = require('alexa-sdk');
 let AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
 let ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 const APP_ID = 'amzn1.ask.skill.1377365e-6b9d-485d-b628-034819fe2c60';
-
 const languageStrings = {
     'en': {
         translation: {
             SKILL_NAME: 'Art gallery',
             WELCOME_MESSAGE: "Welcome to the %s. You can ask a question like, name works by pierre auguste renoir or  name artists like jackson... Whose works would you like to view?",
             WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
-            DISPLAY_CARD_TITLE: '%s  - Matches for %s.',
+            DISPLAY_CARD_TITLE_ARTISTS: 'Artists - %s.',
+            DISPLAY_CARD_TITLE_ARTIST_WORKS: 'Artist Works - %s.',
             HELP_MESSAGE: "You can ask a question like, name works by renoir. ... Now, what can I help you with?",
             HELP_REPROMPT: "You can ask a question like, name works by renoir. ... Now, what can I help you with?",
-            STOP_MESSAGE: 'Goodbye!',
+            STOP_MESSAGE: 'Bye!',
             ITEM_REPEAT_MESSAGE: 'Try saying repeat.',
             ITEM_NOT_FOUND_MESSAGE: "I\'m sorry, I currently do not know ",
             ITEM_NOT_FOUND_WITH_ITEM_NAME: 'the work by %s. ',
             ITEM_NOT_FOUND_WITHOUT_ITEM_NAME: 'that work of art. ',
             ITEM_NOT_FOUND_REPROMPT: 'What else can I help with?',
+            ARTIST_WORK_NO_SPEECH: 'an artist name must be provided to list their works of art',
+            ARTIST_WORK_ERROR: 'there was a problem searching %s please try another artist',
+            ARTIST_WORK_NO_RESULTS: 'There were no works found by %s',
+            ARTIST_NAME_NO_SPEECH: 'an artist name must be provided',
+            ARTIST_NAME_ERROR: 'There was a problem searching %s. Please try another artist name',
+            ARTIST_NAME_NO_RESULTS: 'no artists found like %s'
         },
     },
     'en-US': {
@@ -29,135 +34,84 @@ const languageStrings = {
         },
     }
 };
-
+function sendResponse(context, title, msg, reprompt) {
+    context.attributes.speechOutput = msg;
+    context.attributes.repromptSpeech = reprompt;
+    context.response.speak(msg).listen(context.attributes.repromptSpeech);
+    context.response.cardRenderer(title, msg);
+    context.emit(':responseReady');
+}
+function truncateSpeech(speech) {
+    const alexaSpeechOutputLimit = 7800; // https://stackoverflow.com/questions/36557053/alexa-skill-ssml-max-length
+    if (speech.length > alexaSpeechOutputLimit) {
+        speech = speech.substring(0, alexaSpeechOutputLimit) + '...';
+    }
+    return speech;
+}
 const handlers = {
-    //Use LaunchRequest, instead of NewSession if you want to use the one-shot model
-    // Alexa, ask [my-skill-invocation-name] to (do something)...
+    //Use LaunchRequest, instead of NewSession if you want to use the one-shot model // Alexa, ask [my-skill-invocation-name] to (do something)...
     'LaunchRequest': function () {
         this.attributes.speechOutput = this.t('WELCOME_MESSAGE', this.t('SKILL_NAME'));
-        // If the user either does not reply to the welcome message or says something that is not
-        // understood, they will be prompted again with this text.
         this.attributes.repromptSpeech = this.t('WELCOME_REPROMPT');
         this.response.speak(this.attributes.speechOutput).listen(this.attributes.repromptSpeech);
         this.emit(':responseReady');
     },
     'ArtistWorkIntent': function () {
-        let self = this;
-        const itemSlot = this.event.request.intent.slots.Artist;
-        let itemName;
-        if (itemSlot && itemSlot.value) {
-            itemName = itemSlot.value.toLowerCase();
-        }
+        let self = this; // needed for any nested functions.
+        const itemSlot = self.event.request.intent.slots.Artist;
+        let itemName = itemSlot && itemSlot.value ? itemSlot.value.toLowerCase() : '';
         console.log('searching for ' + itemName);
-        const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
+        const cardTitle = self.t('DISPLAY_CARD_TITLE', self.t('SKILL_NAME'), itemName);
         if (!itemName) {
-            let unfoundResponse = 'an artist name must be provided to list their works of art';
-            self.attributes.speechOutput = unfoundResponse;
-            self.attributes.repromptSpeech = self.t('ITEM_REPEAT_MESSAGE');
-            self.response.speak(unfoundResponse).listen(self.attributes.repromptSpeech);
-            self.response.cardRenderer(cardTitle, unfoundResponse);
-            self.emit(':responseReady');
+            sendResponse(self, cardTitle, self.t('ARTIST_WORK_NO_SPEECH'), self.t('ITEM_NOT_FOUND_REPROMPT'));
+            return;
         }
-        let params = {
-            TableName: 'ImageClassificationV2',
-            ExpressionAttributeValues: {
-                ":artist": {
-                    S: itemName
-                }
-            },
-            KeyConditionExpression: "artist = :artist",
-            IndexName: "ArtistNameIndex"
-        };
-        ddb.query(params, function(err, data) {
+        ddb.query({
+            TableName: 'ImageClassificationV2', ExpressionAttributeValues: { ":artist": { S: itemName } },
+            KeyConditionExpression: "artist = :artist", IndexName: "ArtistNameIndex"
+        }, function(err, data) {
             if (err) {
-                console.log("Error", err);
-            } else {
-                console.log(`found ${data.Items.length} items`);
-                if (data.Items.length > 0) {
-                    let nameAndDateForArtist = data
-                        .Items
-                        .map(item => item.name.S + '(' + item.date.S + ')')
-                        .join(', ');
-                    const alexaSpeechOutputLimit = 7800; // https://stackoverflow.com/questions/36557053/alexa-skill-ssml-max-length
-                    if (nameAndDateForArtist.length > alexaSpeechOutputLimit) {
-                        nameAndDateForArtist = nameAndDateForArtist.substring(0, alexaSpeechOutputLimit) + '...';
-                    }
-                    self.attributes.speechOutput = nameAndDateForArtist;
-                    self.attributes.repromptSpeech = self.t('ITEM_REPEAT_MESSAGE');
-                    self.response.speak(nameAndDateForArtist).listen(self.attributes.repromptSpeech);
-                    self.response.cardRenderer(cardTitle, nameAndDateForArtist);
-                    self.emit(':responseReady');
-                } else {
-                    let speechOutput = self.t('ITEM_NOT_FOUND_MESSAGE');
-                    const repromptSpeech = self.t('ITEM_NOT_FOUND_REPROMPT');
-                    if (itemName) {
-                        speechOutput += self.t('ITEM_NOT_FOUND_WITH_ITEM_NAME', itemName);
-                    } else {
-                        speechOutput += self.t('ITEM_NOT_FOUND_WITHOUT_ITEM_NAME');
-                    }
-                    speechOutput += repromptSpeech;
-                    self.attributes.speechOutput = speechOutput;
-                    self.attributes.repromptSpeech = repromptSpeech;
-                    self.response.speak(speechOutput).listen(repromptSpeech);
-                    self.emit(':responseReady');
-                }
+                sendResponse(self, cardTitle, self.t('ARTIST_WORK_ERROR'), self.t('ITEM_NOT_FOUND_REPROMPT'));
+                return;
             }
+            if (data.Items.length <= 0) {
+                sendResponse(self, cardTitle, self.t('ARTIST_WORK_NO_RESULTS'), self.t('ITEM_NOT_FOUND_REPROMPT'));
+                return;
+            }
+            sendResponse(
+                self,
+                cardTitle,
+                truncateSpeech(
+                    data.Items.map(item => item.name.S + '(' + item.date.S + ')').join(', ')
+                ),
+                self.t('ITEM_REPEAT_MESSAGE')
+            );
         });
     },
     'ArtistIntent': function () {
-        const itemSlot = this.event.request.intent.slots.Artist;
-        let itemName;
-        if (itemSlot && itemSlot.value) {
-            itemName = itemSlot.value.toLowerCase();
-        }
+        let self = this; // needed for any nested functions.
+        const itemSlot = self.event.request.intent.slots.Artist;
+        let itemName = itemSlot && itemSlot.value ? itemSlot.value.toLowerCase() : '';
         console.log('searching for ' + itemName);
-        const cardTitle = this.t('DISPLAY_CARD_TITLE', this.t('SKILL_NAME'), itemName);
-        let params = {
-            TableName: 'ImageArtist',
-            ExpressionAttributeValues: {
-                ":artist": {
-                    S: itemName
-                }
-            },
+        const cardTitle = self.t('DISPLAY_CARD_TITLE', self.t('SKILL_NAME'), itemName);
+        if (!itemName) {
+            sendResponse(self, cardTitle, self.t('ARTIST_NAME_NO_SPEECH'), self.t('ITEM_NOT_FOUND_REPROMPT'));
+            return;
+        }
+        ddb.scan({
+            TableName: 'ImageArtist', ExpressionAttributeValues: { ":artist": { S: itemName } },
             FilterExpression: 'contains(artist, :artist)'
-        };
-        let self = this;
-        ddb.scan(params, function(err, data) {
+        }, function(err, data) {
             if (err) {
-                console.log("Error", err);
-            } else {
-                console.log(`found ${data.Items.length} items`);
-                if (data.Items.length > 0) {
-                    let nameAndDateForArtist = data
-                        .Items
-                        .map(item => item.artist.S)
-                        .join(', ');
-                    const alexaSpeechOutputLimit = 7800; // https://stackoverflow.com/questions/36557053/alexa-skill-ssml-max-length
-                    if (nameAndDateForArtist.length > alexaSpeechOutputLimit) {
-                        nameAndDateForArtist = nameAndDateForArtist.substring(0, alexaSpeechOutputLimit) + '...';
-                    }
-                    self.attributes.speechOutput = nameAndDateForArtist;
-                    self.attributes.repromptSpeech = self.t('ITEM_REPEAT_MESSAGE');
-                    self.response.speak(nameAndDateForArtist).listen(self.attributes.repromptSpeech);
-                    self.response.cardRenderer(cardTitle, nameAndDateForArtist);
-                    self.emit(':responseReady');
-                } else {
-                    let speechOutput = self.t('ITEM_NOT_FOUND_MESSAGE');
-                    const repromptSpeech = self.t('ITEM_NOT_FOUND_REPROMPT');
-                    if (itemName) {
-                        speechOutput += self.t('ITEM_NOT_FOUND_WITH_ITEM_NAME', itemName);
-                    } else {
-                        speechOutput += self.t('ITEM_NOT_FOUND_WITHOUT_ITEM_NAME');
-                    }
-                    speechOutput += repromptSpeech;
-                    self.attributes.speechOutput = speechOutput;
-                    self.attributes.repromptSpeech = repromptSpeech;
-                    self.response.speak(speechOutput).listen(repromptSpeech);
-                    self.emit(':responseReady');
-                }
+                sendResponse(self, cardTitle, self.t('ARTIST_NAME_ERROR'), self.t('ITEM_NOT_FOUND_REPROMPT'));
+                return;
             }
+            if (data.Items.length <= 0) {
+                sendResponse(self, cardTitle, self.t('ARTIST_NAME_NO_RESULTS'), self.t('ITEM_NOT_FOUND_REPROMPT'));
+                return;
+            }
+            sendResponse(self, cardTitle, truncateSpeech(data.Items.map(item => item.artist.S).join(', ')), self.t('ITEM_REPEAT_MESSAGE'));
         });
-
     },
     'AMAZON.HelpIntent': function () {
         this.attributes.speechOutput = this.t('HELP_MESSAGE');
